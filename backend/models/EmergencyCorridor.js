4e6b5d662e68274fa08e7edc66abb8ae2cc3455f
adityakulkarni3618 <adityakulkarni3618@gmail.com>
2026-09-11 00:02:59 +0530

    feat(corridor): extend EmergencyCorridor schema with operational junction fields

--- backend/models/EmergencyCorridor.js
+++ backend/models/EmergencyCorridor.js
@@ -20,12 +20,56 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     status: {
-      type: DataTypes.ENUM('SCHEDULED', 'PREEMPTING', 'CORRIDOR_ACTIVE', 'PASSED'),
-      defaultValue: 'SCHEDULED'
+      type: DataTypes.STRING,
+      defaultValue: 'NORMAL'
+    },
+    route_order: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    distance_from_ambulance: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0
+    },
+    distance_from_start: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0
     },
     eta_seconds: {
       type: DataTypes.INTEGER,
       defaultValue: 0
+    },
+    approach_direction: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    required_movement: {
+      type: DataTypes.STRING,
+      defaultValue: 'Through'
+    },
+    normal_signal_state: {
+      type: DataTypes.STRING,
+      defaultValue: 'RED_CYCLE'
+    },
+    corridor_state: {
+      type: DataTypes.STRING,
+      defaultValue: 'NORMAL'
+    },
+    preemption_requested: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    preemption_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    controller_status: {
+      type: DataTypes.STRING,
+      defaultValue: 'ONLINE'
+    },
+    gps_confidence: {
+      type: DataTypes.STRING,
+      defaultValue: 'HIGH'
     },
     preempt_window_start: {
       type: DataTypes.DATE,
@@ -42,6 +86,10 @@ module.exports = (sequelize) => {
     longitude: {
       type: DataTypes.DOUBLE,
       allowNull: false
+    },
+    last_updated: {
+      type: DataTypes.DATE,
+      allowNull: true
     }
   }, {
     tableName: 'emergency_corridors',
@@ -50,3 +98,4 @@ module.exports = (sequelize) => {
 
   return EmergencyCorridor;
 };
+
